034b72800a400fdef329add2f2d8c74cbd5ff7d3
KenzoRev <kenzorevano18@gmail.com>
2025-08-16 10:25:04 +0700

    Link Navbar

---
+++ cektamsis/resources/js/router/index.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Welcome from '../pages/Welcome.vue'
+import Tentang from '../pages/Tentang.vue'
+import Fitur from '../pages/Fitur.vue'
+import Kontak from '../pages/Kontak.vue'
+
+const routes = [
+  { path: '/', name: 'welcome', component: Welcome },
+  { path: '/tentang', name: 'tentang', component: Tentang },
+  { path: '/fitur', name: 'fitur', component: Fitur },
+  { path: '/kontak', name: 'kontak', component: Kontak },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+export default router
